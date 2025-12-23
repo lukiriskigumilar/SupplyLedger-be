f64fa637b6d8a098d80ebcdf6495d6830e14c437
Luki Riski <contactlukiriski@gmail.com>
2025-12-24 01:41:10 +0700

    feat(auth): add login handler

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -51,3 +51,56 @@ func (h *AuthHandler) RegisterHandler(c *fiber.Ctx) error {
 	)
 
 }
+
+func (h *AuthHandler) LoginHandler(c *fiber.Ctx) error {
+	var req LoginRequestDTO
+	//validate request body
+	if err := c.BodyParser(&req); err != nil {
+		return utils.NewApiResponseError(
+			c,
+			"invalid request",
+			400,
+			fiber.Map{
+				"reason": err.Error(),
+			},
+		)
+	}
+
+	// validate request field
+	if err := req.ValidateRegisterDTO(); err != nil {
+		errors := utils.ParseValidationError(err)
+		return utils.NewApiResponseError(
+			c,
+			"Validation error",
+			400,
+			errors,
+		)
+	}
+
+	// call service
+	result, err := h.service.LoginService(req)
+	if err != nil {
+		return err
+	}
+
+	// SET COOKIES
+	c.Cookie(&fiber.Cookie{
+		Name:     "access-token",
+		Value:    result.Token,
+		Path:     "/",
+		HTTPOnly: true,
+		Secure:   true,
+		SameSite: fiber.CookieSameSiteLaxMode,
+	})
+
+	//RESPONSE
+	response := &LoginResponseDTO{
+		Username:  result.Username,
+		Role:      result.Role,
+		CreatedAt: result.CreatedAt,
+	}
+
+	return utils.NewApiResponseSuccess(
+		c, "Login Successfully", 200, response, nil,
+	)
+}
