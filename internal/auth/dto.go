b7f51c6aa25d2c128feba39bacb68868b4e46a63
Luki Riski <contactlukiriski@gmail.com>
2025-12-24 01:38:29 +0700

    feat(auth): add login DTOs (request, response, result)

--- internal/auth/dto.go
+++ internal/auth/dto.go
@@ -8,6 +8,7 @@ import (
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 )
 
+// DTO REGISTER
 type RegisterRequestDTO struct {
 	Username        string `json:"username"`
 	Password        string `json:"password"`
@@ -44,4 +45,41 @@ type RegisterResponseDTO struct {
 	Id        string    `json:"id"`
 	Username  string    `json:"username"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+//DTO LOGIN
+
+type LoginRequestDTO struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (r LoginRequestDTO) ValidateRegisterDTO() error {
+	return validation.ValidateStruct(
+		&r,
+		validation.Field(&r.Username,
+			validation.Required.Error("Username is required"),
+			validation.Match(regexp.MustCompile(`^[^\s]+$`)).Error("username must not contain space"),
+			validation.Length(4, 100).Error(`Username must be between 4 and 100 characters`),
+		),
+		validation.Field(&r.Password,
+			validation.Required.Error("Password is required"),
+			validation.Length(6, 0).Error("Password must contain at least 6 characters"),
+		),
+	)
+}
+
+type LoginResponseDTO struct {
+	Username  string    `json:"username"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type LoginResult struct {
+	userID    string
+	Username  string
+	Role      string
+	Token     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
