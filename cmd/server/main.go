e111cccf17f8a60389c92828e1656b3963bc6cff
Luki Riski <contactlukiriski@gmail.com>
2025-12-23 19:12:29 +0700

    feat(main): initialize auth and user modules with database and routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/lukiriskigumilar/SupplyLedger-be/internal/auth"
+	"github.com/lukiriskigumilar/SupplyLedger-be/internal/config"
+	httpx "github.com/lukiriskigumilar/SupplyLedger-be/internal/http"
+	"github.com/lukiriskigumilar/SupplyLedger-be/internal/routes"
+	"github.com/lukiriskigumilar/SupplyLedger-be/internal/user"
+)
 
 func main() {
-	app := fiber.New()
+
+	app := fiber.New(
+		fiber.Config{
+			ErrorHandler: httpx.ErrorHandler,
+		},
+	)
+
+	//init db
+	config.ConnectDatabase()
+	db := config.DB
+
+	// init modules
+	userModule := user.InitUserModule(db)
+	authModule := auth.InitAuthModule(userModule)
+
+	api := app.Group("/api/v1")
+	routes.GlobalRoutes(api, authModule)
+
+	//ini routing
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{
