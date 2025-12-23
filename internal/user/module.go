66523e3b75324b8575407c1f147d3592bf786637
Luki Riski <contactlukiriski@gmail.com>
2025-12-24 02:41:11 +0700

    feat(user): initialize user module with service and handler

--- internal/user/module.go
+++ internal/user/module.go
@@ -3,13 +3,19 @@ package user
 import "gorm.io/gorm"
 
 type UserModule struct {
-	UserRepo UserRepository
+	UserRepo    UserRepository
+	UserService UserService
+	UserHandler *UserHandler
 }
 
 func InitUserModule(db *gorm.DB) *UserModule {
 	repo := NewUserRepository(db)
+	service := NewUserService(repo)
+	handler := NewUserHandler(service)
 
 	return &UserModule{
-		UserRepo: repo,
+		UserRepo:    repo,
+		UserService: service,
+		UserHandler: handler,
 	}
 }
