ac7cb708053ec3916a8f5b4411d74fcd0138de72
Luki Riski <contactlukiriski@gmail.com>
2025-12-24 02:43:25 +0700

    feat(routes): add user routes to global routes

--- internal/routes/global_routes.go
+++ internal/routes/global_routes.go
@@ -3,10 +3,13 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/lukiriskigumilar/SupplyLedger-be/internal/auth"
+	"github.com/lukiriskigumilar/SupplyLedger-be/internal/user"
 )
 
 func GlobalRoutes(api fiber.Router,
 	authModule *auth.AuthModule,
+	userModule *user.UserModule,
 ) {
 	auth.AuthRoute(api, authModule)
+	user.UserRoute(api, userModule)
 }
