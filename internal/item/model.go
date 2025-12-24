a2160ab0dec887c1f7ea3282dd0d1dabefe78cff
Luki Riski <contactlukiriski@gmail.com>
2025-12-24 21:33:21 +0700

    refactor(item) : change type time.Time in delete to gorm.deletedAt

--- internal/item/model.go
+++ internal/item/model.go
@@ -4,14 +4,15 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Item struct {
-	ID        uuid.UUID
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey"`
 	Name      string
 	Stock     int
 	Price     int
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	DeletedAt *time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
